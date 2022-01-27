bb9719ae26e3a9a6cc9b574d0138a577cf2fc350
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-26 17:56:24 -0700

    template for POSTing card

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,20 @@ var authenticationSuccess = function() {
     success: authenticationSuccess,
     error: authenticationFailure
   });
+
+  var myList = 'INSERT YOUR IDLIST HERE';
+
+var creationSuccess = function (data) {
+  console.log('Card created successfully.');
+  console.log(JSON.stringify(data, null, 2));
+};
+
+var newCard = {
+  name: 'New Test Card',
+  desc: 'This is the description of our new card.',
+  // Place this card at the top of our list
+  idList: myList,
+  pos: 'top'
+};
+
+window.Trello.post('/cards/', newCard, creationSuccess);
