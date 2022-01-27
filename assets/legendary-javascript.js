7d1706b663b75ec89d2cca705c48c54d346046f1
Nicholas Tatom <94080507+tatomtech@users.noreply.github.com>
2022-01-26 19:25:33 -0700

    Update legendary-javascript.js
    adding back end teams code

---
+++ assets/legendary-javascript.js
@@ -0,0 +1,41 @@
+//determine what happens once auth is finished
+
+var authenticationSuccess = function() {
+    console.log('Successful authentication');
+  };
+  
+  var authenticationFailure = function() {
+    console.log('Failed authentication');
+  };
+
+  //authorization and authentication
+
+  window.Trello.authorize({
+    type: 'popup',
+    name: 'Getting Started Application',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: 'never',
+    success: authenticationSuccess,
+    error: authenticationFailure
+  });
+
+  //POST template
+
+var myList = '61f1d40a93852c1b9f88152a';
+
+var creationSuccess = function (data) {
+  console.log('Card created successfully.');
+  console.log(JSON.stringify(data, null, 2));
+};
+
+var newCard = {
+  name: 'New Test Card',
+  desc: 'This is the description of our new card.',
+  // Place this card at the top of our list
+  idList: myList,
+  pos: 'top'
+};
+
+window.Trello.post('/cards/', newCard, creationSuccess);
