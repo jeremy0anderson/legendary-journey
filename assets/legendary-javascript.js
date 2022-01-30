c9e825567febdaaf6cbe809c4260289de8a4b3d8
jeremy0anderson <96454777+jeremy0anderson@users.noreply.github.com>
2022-01-30 16:19:51 -0700

    Update legendary-javascript.js

--- assets/legendary-javascript.js
+++ assets/legendary-javascript.js
@@ -1,4 +1,3 @@
-Trello.authorize(authorizeOptions);
 let dateField = document.querySelector("#date-1643251044760");
 let nameField = document.querySelector("#text-1643251049905");
 let phoneField = document.querySelector("#text-1643251062289");
@@ -26,6 +25,7 @@ let card = {
      idList: '61f7164aea56070edecb617e',
      keepFromSource: 'all'
 }
+Trello.authorize(o);
 submit.addEventListener('click', async() => {
 newCard.name = nameField.value + " requested a meeting on " + dateField.value;
 newCard.desc = nameField.value + " included the message: "+ messageField.value + ". Contact them at: "+phoneField.value+ " or "+emailField.value;
